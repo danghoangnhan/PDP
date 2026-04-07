3e45e2e5f766168b9bf171a408f1f5ad1ad28ee9
Daniel Du <danghoangnhan.1@gmail.com>
2026-04-07 17:30:57 +0800

    experiment(Math): remove __main__ test block from distance.py

--- Math/distance.py
+++ Math/distance.py
@@ -7,6 +7,3 @@ def distance(lat1, lon1, lat2, lon2):
     a = 0.5 - math.cos((lat2 - lat1) * p) / 2 + math.cos(lat1 * p) * math.cos(lat2 * p) * (
             1 - math.cos((lon2 - lon1) * p)) / 2
     return 12742 * math.asin(math.sqrt(a))  # 2*R*asin...
-
-if __name__ == "__main__":
-    print(distance(23.69781, 120.96052, 14.05832, 108.27720))
