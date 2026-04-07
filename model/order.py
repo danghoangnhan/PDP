e96bcb8836276afdba840a8e7f995f02d9ab0864
Daniel Du <danghoangnhan.1@gmail.com>
2026-04-07 17:10:04 +0800

    experiment(model): remove unused imports and methods from Ds class

--- model/order.py
+++ model/order.py
@@ -1,14 +1,5 @@
-from pandocfilters import Null
-from sqlalchemy import null
-
-
 class Ds:
     def __init__(self, orderId, timeRequest, restaurantId, vehicle, L, x, y, deadline):
-        # t is the request's time
-        # R is the ordered restaurant
-        # V is the assigned driver
-        # L is the status
-
         self.orderId = orderId
         self.t = timeRequest
         self.V = vehicle
@@ -17,21 +8,14 @@ class Ds:
         self.y: float = y
         self.deadline = deadline
         self.restaurantId = restaurantId
-        self.deadline = deadline
         self.arriveTime = 0
-        self.driverId = Null
+        self.driverId = None
 
     def get_timeRequest(self):
         return self.t
 
-    def getVehicle(self):
-        return self.V
-
     def getLongitude(self):
         return self.y
-
-    def getAtitude(self):
-        return self.x
 
     def getLatitude(self):
         return self.x
@@ -42,30 +26,6 @@ class Ds:
     def getRestaurantId(self):
         return self.restaurantId
 
-    def setRestautantId(self, restaurantId: int):
-        self.restaurantId = restaurantId
-
-    def setDriver(self, driverId: int):
-        self.V = driverId
-
-    def setArriveTime(self, arriveTime):
-        self.arriveTime = arriveTime
-
-    def getArriveTime(self):
-        return self.arriveTime
-
-    def setDeadline(self, time: int):
-        self.time = time
-
-    def setVelocity(self, velocity):
-        self.velocity = velocity
-
-    def getVelocity(self):
-        return self.velocity
-
-    def setOrderId(self, orderId: int):
-        self.orderId = orderId
-
     def setDriverId(self, driverId: int):
         self.driverId = driverId
 
@@ -74,6 +34,3 @@ class Ds:
 
     def getId(self):
         return self.orderId
-
-    def setId(self, id: int):
-        self.orderId = id
